0ab444197159dc2ffb51d4284f6ae3ca06e3c980
NosborCT <hainiken08@gmail.com>
2024-05-04 00:54:03 -0300

    feat: add search component

---
+++ app/_components/search.tsx
@@ -0,0 +1,20 @@
+import { Input } from "@/app/_components/ui/input";
+import { Button } from "./ui/button";
+import { SearchIcon } from "lucide-react";
+
+const Search = () => {
+    return ( 
+        <div className="flex gap-2 ">
+            <Input placeholder="Buscar restaurantes" className="border-none"/>
+            <Button size={'icon'} > 
+            <SearchIcon size={20}/>
+            </Button>
+
+        </div>
+
+     );
+}
+    <div className="flex gap-4"></div>
+
+ 
+export default Search;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
 
 
 import Header from "./_components/header";
+import Search from "./_components/search";
 
 export default function Home() {
   return ( 
+    <>
     <Header/>
+    
+    <div className="px-5 py-6" >
+      <Search/>
+    </div>
+    </>
   );
 }
