f8224e12382f559bb82aa723d6620f8cbdf7f6b0
NosborCT <hainiken08@gmail.com>
2024-05-06 23:43:04 -0300

    fix: header components

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
 
     return ( 
         <div className='flex justify-between pt-6 px-5'>
-        <Image src = "/logo.png" alt = "FSW Foods" height = {30} width={100} />
+        <Image src = "/logo.png" alt = "FSW Foods" height = {60} width={100}/>
 
         <Button size = 'icon' variant={'outline'} className='border-none bg-transparent'>
             <MenuIcon/>
