11bc414d600f86e7cfcb0e38d77b77dd117c970a
NosborCT <hainiken08@gmail.com>
2024-05-30 00:29:03 -0300

    Feat: Back to home onClick logo Image

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,12 +1,19 @@
+"use client";
 import Image  from 'next/image';
 import { Button } from './ui/button';
 import { MenuIcon } from 'lucide-react';
+import { useRouter } from 'next/navigation';
 
 const Header = () => {
 
+    const router = useRouter();
+    const handleBackClick = () => router.back()
+    
     return ( 
         <div className='flex justify-between pt-6 px-5'>
-        <Image src = "/logo.png" alt = "FSW Foods" height = {60} width={100}/>
+
+
+        <Image src = "/logo.png" alt = "FSW Foods" height = {60} width={100} onClick={handleBackClick}/>
 
         <Button size = 'icon' variant={'outline'} className='border-none bg-transparent'>
             <MenuIcon/>
