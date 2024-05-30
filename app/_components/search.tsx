3c5a798411d827834806ee212db64a7dfc54d408
NosborCT <hainiken08@gmail.com>
2024-05-30 02:45:11 -0300

    Feat : Search

--- app/_components/search.tsx
+++ app/_components/search.tsx
@@ -1,20 +1,43 @@
+"use client";
+
 import { Input } from "@/app/_components/ui/input";
 import { Button } from "./ui/button";
 import { SearchIcon } from "lucide-react";
+import { useState, FormEventHandler } from "react";
+import { useRouter } from 'next/navigation';
 
 const Search = () => {
-    return ( 
-        <div className="flex gap-2 ">
-            <Input placeholder="Buscar restaurantes" className="border-none"/>
-            <Button size={'icon'} > 
-            <SearchIcon size={20}/>
-            </Button>
 
-        </div>
+    const router = useRouter();
+    const [search, setSearch] = useState("");
+  
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setSearch(e.target.value);
+    };
+  
+    const handleSearchSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+      e.preventDefault();
+  
+      if (!search) {
+        return;
+      }
+  
+      router.push(`/restaurant?search=${search}`);
+    };
 
-     );
-}
-    <div className="flex gap-4"></div>
-
- 
-export default Search;
+    return (
+        <form className="flex gap-2" onSubmit={handleSearchSubmit}>
+          <Input
+            placeholder="Buscar restaurantes"
+            className="border-none"
+            onChange={handleChange}
+            value={search}
+          />
+          <Button size="icon" type="submit">
+            <SearchIcon size={20} />
+          </Button>
+        </form>
+      );
+    };
+    
+    export default Search;
