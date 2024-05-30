72793ac49a9b2b9110931d561b381ccc78c6a705
NosborCT <hainiken08@gmail.com>
2024-05-29 22:52:33 -0300

    Fix : Add button to `Adicionar a sacola`

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -2,6 +2,7 @@ import { db } from "@/app/_lib/prisma";
 import { notFound } from "next/navigation";
 import ProductImage from "./_components/product-image";
 import ProductDetails from "./_components/product-details";
+import { Button } from "@/app/_components/ui/button";
 
 
 interface ProductPageProps{
@@ -45,9 +46,15 @@ const ProductPage = async ({params:{id}}:ProductPageProps) => {
         {/* Titulo e Preço do produto */}
        <ProductDetails product={product} complementaryProducts={[]}/>
 
-        
+      <div className="px5">
+        <Button className="w-full mt-3">Adicionar a sacola</Button>
+      </div>
+
+
+
     </div>
 
+      
 
     );  
     
